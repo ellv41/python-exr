54f90bf5fa983af1d7bacf000eb2f307dfe226ea
Eli Levy <eli4146@gmail.com>
2023-03-15 17:00:53 +0200

    tic tac toe game  - added remarks

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -4,8 +4,11 @@
 #         self.size = size
 import time
 
-
+# only playing on 3 X 3 board
 EMPTY_BOARD = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+# the first 3 items  represent rows 1 - 3 in the board  ,  the next 3  1 - 3 columns
+# the 7 position is for diagonal left to right and 8 pos for the diagonal right to left
 VICTORY_X = [0, 0, 0, 0, 0, 0, 0, 0]
 VICTORY_O = [0, 0, 0, 0, 0, 0, 0, 0]
 PLAYER1 = 'X'
@@ -25,30 +28,30 @@ LOGO = """
 """
 
 
-def check_board(board, player):
+def check_board(board, playr):
     global VICTORY_X
     global VICTORY_O
     full = 0
     victory = [0, 0, 0, 0, 0, 0, 0, 0]
     b_size = len(board)
     for i in range(0, b_size):
-        victory[i] = board[i].count(player)
+        victory[i] = board[i].count(playr)
         full += board[i].count('_')
-        if board[i][i] == player:
+        if board[i][i] == playr:
             victory[6] += 1
         for j in range(0, b_size):
-            if board[i][j] == player:
+            if board[i][j] == playr:
                 victory[j+3] += 1
     j = b_size - 1
     for i in range(0, b_size):
-        if board[j][i] == player:
+        if board[j][i] == playr:
             victory[7] += 1
         j -= 1
-    if player == 'X':
+    if playr == 'X':
         VICTORY_X = victory
     else:
         VICTORY_O = victory
-    print(f'player{player} victory = {VICTORY_X}') if player == 'X' else print(f'player{player} victory = {VICTORY_O}')
+    print(f'player{playr} victory = {VICTORY_X}') if playr == 'X' else print(f'player{playr} victory = {VICTORY_O}')
     print(f'full = {full}')
     for i in range(0, 8):
         if VICTORY_X[i] >= b_size:
@@ -65,6 +68,7 @@ def print_bord(bord):
         print(f'|  {bord[i][0]}  |  {bord[i][1]}  |  {bord[i][2]}  |\n')
 
 
+# FUNC for input from player for row an col
 def play_move(bord, plyer):
     legal = False
     while not legal:
@@ -80,7 +84,7 @@ def play_move(bord, plyer):
     return status
 
 
-# Main #
+# ****     Main   *******  #
 print(LOGO)
 bord1 = EMPTY_BOARD
 game_over = False
@@ -99,8 +103,11 @@ while not game_over:
         print(f'\n ****** And the Winner is ********* player-{game_status}')
     if game_status == 'D':
         print(f'no more moves the board is full')
+        # X = player X wins O plyer O wins and D for draw
     if game_status in ('X', 'O', 'D'):
         bord1 = EMPTY_BOARD
+        # the first 3 items  represent rows 1 - 3 in the board  ,  the next 3  1 - 3 columns
+        # the 7 position is for diagonal left to right and 8 pos for the diagonal right to left
         VICTORY_X = [0, 0, 0, 0, 0, 0, 0, 0]
         VICTORY_O = [0, 0, 0, 0, 0, 0, 0, 0]
         tmp1 = input('want another game press Y : ')
